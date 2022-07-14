c65e073dcd4b6bf3f625de9cb8e7e92bdbb362c0
TheDevick <erick@bilhalba.com.br>
2022-07-14 14:57:59 -0300

    Refactor Repository Class

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -3,13 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Entity;
+use Doctrine\Persistence\ObjectRepository;
 
-interface Repository
+interface Repository extends ObjectRepository
 {
-    public function findAll();
-    public function findBy(array $criteria, ?array $orderBy = null, $limit = null, $offset = null);
-    public function find($id, $lockMode = null, $lockVersion = null);
-
     public function add(Entity $entity, bool $flush = false): void;
     public function remove(Entity $entity, bool $flush = false): void;
     public function flush(): void;
