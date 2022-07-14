30fa58a822e18032ad1719879074dad369aa1080
TheDevick <erick@bilhalba.com.br>
2022-07-13 23:38:28 -0300

    Update Doctor Factory

--- src/Factory/DoctorFactory.php
+++ src/Factory/DoctorFactory.php
@@ -3,14 +3,20 @@
 namespace App\Factory;
 
 use App\Entity\Doctor;
+use App\Repository\SpecialtyRepository;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\Response;
 
 class DoctorFactory
 {
-    public array $doctorRequiredElements = ['Name', 'Area', 'Subscription'];
-    public array $doctorAllElements = ['Name', 'Area', 'Subscription'];
+    public function __construct(private SpecialtyRepository $specialtyRepository)
+    {
+    }
 
-    public function checkArrayToCreateDoctor(array $array): bool
+    public array $doctorRequiredElements = ['Name', 'Area', 'Subscription', 'SpecialtyId'];
+    public array $doctorAllElements = ['Name', 'Area', 'Subscription', 'SpecialtyId'];
+
+    public function checkArrayToCreateDoctor(array $array): array|bool
     {
         $collection = new ArrayCollection($array);
 
@@ -18,8 +24,31 @@ class DoctorFactory
             $arrayContains = $collection->containsKey($value);
 
             if (!$arrayContains) {
-                return false;
+                return [
+                    'Error' => 'This Resource is Missing Parameters',
+                    'Status Code' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                ];
             }
+        }
+
+        if (!$this->checkSpecialtyExists($array)) {
+            return [
+                'Error' => 'Specialty Resorce Not Found',
+                'Status Code' => Response::HTTP_NOT_FOUND,
+            ];
+        }
+
+        return true;
+    }
+
+    private function checkSpecialtyExists(array $data): bool
+    {
+        $specialtyId = $data['SpecialtyId'];
+
+        $specialty = $this->specialtyRepository->find($specialtyId);
+
+        if (is_null($specialty)) {
+            return false;
         }
 
         return true;
@@ -32,12 +61,29 @@ class DoctorFactory
         $doctor->$functionName($value);
     }
 
+    private function setSpecialty(Doctor $doctor, int $id)
+    {
+        $specialty = $this->specialtyRepository->find($id);
+
+        $doctor->setSpecialty($specialty);
+
+        $this->specialtyRepository->add($specialty, true);
+    }
+
     private function doctorSetValues(Doctor $doctor, array $values): Doctor
     {
         $collection = new ArrayCollection($values);
 
         foreach ($this->doctorAllElements as $element) {
             $arrayContains = $collection->containsKey($element);
+
+            if ('SpecialtyId' == $element) {
+                $specialtyId = $collection->get($element);
+
+                $this->setSpecialty($doctor, $specialtyId);
+
+                continue;
+            }
 
             if ($arrayContains) {
                 $value = $collection->get($element);
@@ -52,6 +98,10 @@ class DoctorFactory
     public function createDoctor(array $data): Doctor|false
     {
         if (!$this->checkArrayToCreateDoctor($data)) {
+            return false;
+        }
+
+        if (!$this->checkSpecialtyExists($data)) {
             return false;
         }
 
