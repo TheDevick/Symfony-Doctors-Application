0ced691ea84ebff806595e4101ffeff6adbb4b69
TheDevick <erick@bilhalba.com.br>
2022-07-13 23:02:27 -0300

    Create Store Action in Specialty Controller

--- src/Controller/SpecialtyController.php
+++ src/Controller/SpecialtyController.php
@@ -2,12 +2,38 @@
 
 namespace App\Controller;
 
+use App\Factory\SpecialtyFactory;
+use App\Repository\SpecialtyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SpecialtyController extends AbstractController
 {
+    public function __construct(
+        private SpecialtyRepository $specialtyRepository,
+        private SpecialtyFactory $specialtyFactory
+    ) {
+    }
+
+    private function jsonResponseNotFound(): JsonResponse
+    {
+        $error = ['Error' => 'No Resources Found'];
+        $statusCode = Response::HTTP_NOT_FOUND;
+
+        return new JsonResponse($error, $statusCode);
+    }
+
+    private function jsonResponseMissingParameters(): JsonResponse
+    {
+        $error = ['Error' => 'This Resource is Missing Parameters'];
+        $statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
+
+        return new JsonResponse($error, $statusCode);
+    }
+
     #[Route(path: '/specialties', name: 'specialties.index', methods: 'GET')]
     public function index(): JsonResponse
     {
@@ -19,13 +45,21 @@ class SpecialtyController extends AbstractController
     }
 
     #[Route(path: '/specialties', name: 'specialties.store', methods: 'POST')]
-    public function store(): JsonResponse
+    public function store(Request $request): JsonResponse
     {
-        return new JsonResponse([
-            'Path' => '/specialties',
-            'Name' => 'Specialties.Store',
-            'Methods' => 'POST',
-        ]);
+        $data = $request->toArray();
+
+        $checks = $this->specialtyFactory->checkArrayToCreateSpecialty($data);
+
+        if (!$checks) {
+            return $this->jsonResponseMissingParameters();
+        }
+
+        $specialty = $this->specialtyFactory->createSpecialty($data);
+
+        $this->specialtyRepository->add($specialty, true);
+
+        return new JsonResponse($specialty);
     }
 
     #[Route(path: '/specialties/{id}', name: 'specialties.show', methods: 'GET')]
