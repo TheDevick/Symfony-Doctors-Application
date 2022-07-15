b1af5f3efdd02f464f0c9a624ddf08ba54350e00
TheDevick <erick@bilhalba.com.br>
2022-07-15 15:35:45 -0300

    Fix Entity Class Styles

--- src/Entity/Entity.php
+++ src/Entity/Entity.php
@@ -5,7 +5,9 @@ namespace App\Entity;
 abstract class Entity implements \JsonSerializable
 {
     abstract public static function elementsToCreate(): array;
+
     abstract public function getId(): ?int;
+
     abstract public function view(): array;
 
     public function jsonSerialize(): mixed
