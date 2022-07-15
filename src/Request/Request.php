ba40f0c874d55be24489de4c372307f90f4fb929
TheDevick <erick@bilhalba.com.br>
2022-07-15 14:06:51 -0300

    Add Create Request Function on Custom Request

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -6,5 +6,8 @@ use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 
 class Request extends HttpFoundationRequest
 {
-    
+    public static function createRequest(): Request
+    {
+        return Request::createFromGlobals();
+    }
 }
