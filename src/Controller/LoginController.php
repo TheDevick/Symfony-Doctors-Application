e8bfe3aa8ad0ec85b849d1f259f77d5239dbe269
TheDevick <erick@bilhalba.com.br>
2022-07-16 16:09:17 -0300

    Add Verification To Email and Passoword in Body

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Request\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
@@ -22,5 +24,23 @@ class LoginController extends AbstractController
         $statusCode = Response::HTTP_UNAUTHORIZED;
 
         return new JsonResponse($message, $statusCode);
+    }
+
+    #[Route(path: '/login', name: 'users.login', methods: ['POST'])]
+    public function login()
+    {
+        $request = Request::createRequest();
+
+        $email = $request->getParameterBody('Email');
+        $password = $request->getParameterBody('Password');
+
+        if (is_null($email) || is_null($password)) {
+            $message = ['Error' => 'This Resource is Missing Parameters'];
+            $statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
+
+            return new JsonResponse($message, $statusCode);
+        }
+
+        return new JsonResponse(['Success' => 'Login is Correct']);
     }
 }
