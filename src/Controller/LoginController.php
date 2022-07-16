5b942aa9cea1929577d7ddf7e6b27235d37245f3
TheDevick <erick@bilhalba.com.br>
2022-07-16 16:07:55 -0300

    Add Email or Password Incorrect Method

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class LoginController extends AbstractController
@@ -12,5 +14,13 @@ class LoginController extends AbstractController
         private UserRepository $userRepository,
         private UserPasswordHasherInterface $passwordHasher
     ) {
+    }
+
+    private function emailOrPasswordIncorrect(): JsonResponse
+    {
+        $message = ['Error' => 'Email or Password Incorrect'];
+        $statusCode = Response::HTTP_UNAUTHORIZED;
+
+        return new JsonResponse($message, $statusCode);
     }
 }
