b661e297b2091abbd2e563ee6fd71f91cf8163f3
TheDevick <erick@bilhalba.com.br>
2022-07-16 00:20:21 -0300

    Create App Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\DoctorFactory;
+use App\Factory\SpecialtyFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,12 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        SpecialtyFactory::createMany(20);
+        DoctorFactory::createMany(20);
+
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
+        // $manager->flush();
     }
 }
